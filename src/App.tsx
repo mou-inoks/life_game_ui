eef277f1b09d54b0026b169f73acb4e8f687b56e
mou-inoks <mou@inokscapital.com>
2022-05-17 22:47:05 +0200

    Add formulaire and place the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Formulaire from './Components/Formulaire';
 import  Tableau from './Components/Tableau';
 import {Box} from "@mui/material"
 function App() {
   return (
     <Box className="App">
+      <Formulaire />
       <Tableau />
     </Box>
   );
--- src/Components/Formulaire.tsx
+++ src/Components/Formulaire.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import axios from 'axios';
+import './Formulaire.css'
 
 export default function Formulaire() {
   return (
     <div className="form">
     <div className="title">Bienvenu</div>
-    <div className="subtitle">Entrez les paramètres</div>
+    <div className="subtitle">Choisissez les paramêtres du point</div>
     <div className="input-container ic1">
       <input id="segmentsize" className="input" type="text" placeholder=" "  />
       <div className="cut"></div>
-      <label htmlfor="texte" className="placeholder">Taille du Segment (px)</label>
+      <label  className="placeholder">X</label>
     </div>
     <div className="input-container ic2">
       <input id="degre" className="input" type="text" placeholder=" "  />
       <div className="cut"></div>
-      <label className="placeholder">Degré du fractale</label>
+      <label className="placeholder">Y</label>
     </div>
-    <button type="text" className="submit">Valider</button>
+    <button  className="submit">Valider</button>
 
     <div className="input-container ic2">
         {}
