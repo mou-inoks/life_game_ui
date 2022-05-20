1906b19573fbda3c0488e0c8258b783a8bcbd1d7
Salim Mouttaki <MOU@inokscapital.com>
2022-05-20 14:44:59 +0200

    FINISHED THE LIFEGAME

--- src/Components/Tableau.tsx
+++ src/Components/Tableau.tsx
@@ -1,44 +1,46 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import './tableau.css'
-import Formulaire from './Formulaire';
 
+async function callApi(counter: number) {
+    const requestOption = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: { counter: Number(counter) }
+    }
+    console.log("compteur:", counter)
+    let response = await axios.post('https://localhost:7003/api/LifeGame/' + counter)
+    return response.data
+}
 
 const Tableau = () => {
-   
-    var [tableau, setTableau] = useState(new Array);
 
-    var [counter, setCounter] = useState(0);
+    var [tableau, setTableau] = useState([]);
 
-    async function ApiRequest(){
-        const requestOption = {
-          method: 'POST', 
-          headers:{'Content-Type': 'application/json'},
-          body: {counter}
+    async function handleApiRequestButton() {
+
+        for (let i = 0; i < 1000; i++) {
+            await callApi(i).then((data) => setTableau(data))
         }
-        setCounter(counter++)
-        let response = await axios.post('https://localhost:7003/api/LifeGame', requestOption)
-        
-        setTableau(response.data);
-      }
-    
-    if(tableau === null)
+
+    }
+
+    if (tableau === null)
         return null;
-    else{
+    else {
         return (<>
-        <Formulaire/>
-        <button onClick={() => ApiRequest} className="submit">Placer</button>
-        <svg className='test' width={900} height={900}>
-            {tableau.map((row: Array<0 | 1>, rowIndex : number) => {
-                return row.map((column: 0 | 1, columnIndex: number)  => {
-                    return(
-                        <Rectangle x={50*rowIndex } y={50 * columnIndex } width={50} height={50} alive={column}/>
-                    ) 
-                })
-            })}
-        </svg> 
+            <button onClick={() => handleApiRequestButton()} className="submit">Placer</button>
+            <svg className='test' width={900} height={900}>
+                {tableau.map((row: Array<0 | 1>, rowIndex: number) => {
+                    return row.map((column: 0 | 1, columnIndex: number) => {
+                        return (
+                            <Rectangle x={25 * rowIndex} y={25 * columnIndex} width={25} height={25} alive={column} />
+                        )
+                    })
+                })}
+            </svg>
         </>
-        )}
+        )
+    }
 }
 export interface Cellules {
     x: number,
@@ -52,7 +54,7 @@ const Rectangle = (props: Cellules) => {
     let color = props.alive === 1 ? "black" : "white"
 
     return (
-    <rect {...props} fill={color}/>
+        <rect {...props} fill={color} />
     )
 }
 export default Tableau
